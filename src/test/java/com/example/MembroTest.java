75540d4bcaafa091ae9ce9762c37fb95a5a5463d
Vitor Stahlberg <vitorlberg@gmail.com>
2023-09-06 15:44:06 -0300

    test: successful Membro tests

--- src/test/java/com/example/MembroTest.java
+++ src/test/java/com/example/MembroTest.java
@@ -1,4 +1,43 @@
 package com.example;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.example.entities.Livro;
+import org.example.entities.Membro;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class MembroTest {
+
+    @Test
+    @DisplayName("Verifica se um membro pode ser criado corretamente")
+    public void testCriarMembro() {
+        new Membro("Gustavo");
+    }
+
+    @Test
+    @DisplayName("Verifica se um membro pode pegar um livro emprestado corretamente")
+    public void testMembroPegaEmprestado() {
+        Membro membro = new Membro("Joao");
+        Livro livro1 = new Livro("bla", "aaaaaa");
+        membro.addLivrosEmprestados(livro1);
+        Livro livro2 = membro.getLivrosEmprestados().stream().filter(x -> x.getTitulo() == livro1.getTitulo())
+                .findFirst().orElse(null);
+
+        assertEquals(livro1, livro2);
+    }
+
+    @Test
+    @DisplayName("Verifica se um membro pode pegar um livro emprestado corretamente")
+    public void testMembroRetornaLivro() {
+        Membro membro = new Membro("Joao");
+        Livro livro1 = new Livro("bla", "aaaaaa");
+        membro.addLivrosEmprestados(livro1);
+        membro.devolverLivro(livro1);
+        Livro livro2 = membro.getLivrosEmprestados().stream().filter(x -> x.getTitulo() == livro1.getTitulo())
+                .findFirst().orElse(null);
+
+        assertEquals(livro2, null);
+    }
+
 }
